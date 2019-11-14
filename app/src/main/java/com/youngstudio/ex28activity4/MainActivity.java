cd23d057f25528e7cd511a848274b507c649cb77
rmflawk <ant921111@naver.com>
2019-11-14 17:46:10 +0900

    1

---
+++ app/src/main/java/com/youngstudio/ex28activity4/MainActivity.java
@@ -0,0 +1,26 @@
+package com.youngstudio.ex28activity4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void clickBtn(View view) {
+        //묵시적(implicit) Intent를 통해 SecondActivity 실행
+        Intent intent= new Intent();
+        //intent.setAction("aaa");
+        intent.setAction("bbb");
+        startActivity(intent);
+
+    }
+}
